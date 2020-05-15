44a1e40fa6b9640fa9a0ee7036e3e9fa9f3d8c7b
Nadine-Amr <s-nadine.amr@zewailcity.edu.eg>
2020-05-15 03:38:37 +0200

    Adding adjustable communication range and some new arguments in launch files.

--- simulator/racecar-simulator/racecar_communication/src/rec_id_msg.py
+++ simulator/racecar-simulator/racecar_communication/src/rec_id_msg.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Polygon
 from racecar_communication.msg import IDStamped, FootprintsCombined, ID, IDsCombined
 
@@ -7,6 +9,11 @@ class RecIDMsgs:
     def __init__(self):
         
         self.robot_num = rospy.get_param('robot_num')
+        self.comm_range = rospy.get_param('comm_range')
+
+        self.x_position = 0
+        self.y_position = 0
+
         self.last_received_footprints = FootprintsCombined()
         self.last_received_ids = self.initIdsCombined()
 
@@ -25,6 +32,13 @@ class RecIDMsgs:
 
         return idsCombined_msg
 
+
+    # Input data is Odometry message from topic /vesc/odom
+    def odometryCallback(self, odomMsg):
+
+        self.x_position = odomMsg.pose.pose.position.x;
+        self.y_position = odomMsg.pose.pose.position.y;
+
     
     def idMsgsCallback(self, idMsgs):
         if (idMsgs.robot_num != self.robot_num):
@@ -34,7 +48,15 @@ class RecIDMsgs:
                 self.last_received_ids.header.frame_id = "map";
                 self.last_received_ids.robot_num = self.robot_num
 
-                self.last_received_ids.ids[idMsgs.robot_num - 1] = idMsgs.id
+                # Check if the sending vehicle is within the communication range
+                dist_from_ego = np.linalg.norm(np.array([self.x_position - idMsgs.id.x_position, self.y_position - idMsgs.id.y_position]))
+
+                if (dist_from_ego <= self.comm_range):
+                    self.last_received_ids.ids[idMsgs.robot_num - 1] = idMsgs.id
+                else:
+                    self.last_received_ids.ids[idMsgs.robot_num - 1] = ID()
+                    self.last_received_ids.ids[idMsgs.robot_num - 1].lane_num = -1
+
 
                 self.pub1.publish(self.last_received_ids)
 
@@ -47,7 +69,7 @@ class RecIDMsgs:
                 self.last_received_footprints.footprints[idMsgs.robot_num - 1] = idMsgs.id.footprint
 
                 if (idMsgs.id.footprint.points != []):
-	                self.pub2.publish(self.last_received_footprints)
+                    self.pub2.publish(self.last_received_footprints)
 
 
 if __name__ == '__main__':
@@ -58,4 +80,6 @@ if __name__ == '__main__':
 
     # subscribe to '/id_msgs'
     rospy.Subscriber('/id_msgs', IDStamped, rim.idMsgsCallback, queue_size=50)
+    # subscribe to '/vesc/odom'
+    rospy.Subscriber('/vesc/odom', Odometry, rim.odometryCallback, queue_size=50)
     rospy.spin()
