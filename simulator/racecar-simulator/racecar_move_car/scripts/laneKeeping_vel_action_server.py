efe2e5f87af05aed5888948528813f1763c8ef4c
Nadine-Amr <s-nadine.amr@zewailcity.edu.eg>
2020-05-29 18:04:38 +0200

    Blocking servers while action execution is ongoing.

--- simulator/racecar-simulator/racecar_move_car/scripts/laneKeeping_vel_action_server.py
+++ simulator/racecar-simulator/racecar_move_car/scripts/laneKeeping_vel_action_server.py
@@ -25,9 +25,8 @@ class KraussModel:
     def __init__(self):
 
         self.t_r = 1 # estimated driver reaction time ~ 1 sec
-        #self.max_vel = 0.5 # maximum ego vehicle velocity allowed 
-        self.max_vel = 0.2 # testing  
-        self.max_acc = 0.1 # maximum ego vehicle acceleration allowed
+        self.max_vel = 0.5 # maximum ego vehicle velocity allowed
+        self.max_acc = 0.05 # maximum ego vehicle acceleration allowed
         self.vehicle_length = 0.58 # length of vehicles - please adjust this value if another vehicle model is used
 
         self.vel = 0 # ego vehicle current velocity
@@ -41,8 +40,12 @@ class KraussModel:
         self.lead_gap = 0 # gap between leading and ego vehicles
 
         self.safe_vel = 0 # calculated safe velocity for ego vehicle based on Krauss model
+        self.ceil_vel = 0 # highest velocity possible at this time step
+        self.floor_vel = 0 # lowest velocity possible at this time step
+
         self.req_nav_vel = 0 # velocity requested by navigaion module
         self.req_RL_acc = 0 # velocity requested by RL policy
+        self.RL_acc_count = 0 # number of times acceleration/deceleration was feasible within one RL acceleration request
 
         self.last_time = 0 # time of last iteration in seconds
         self.del_time = 1 # chosen delta time between iterations
@@ -111,7 +114,7 @@ class KraussModel:
         # calculate current delta time
         curr_del_time = rospy.Time.now().secs - self.last_time      
 
-        if ((self.active == 2) or ((self.active == 1) and (curr_del_time >= self.del_time))):
+        if ( ((self.active == 2) or (self.active == 1)) and (curr_del_time >= self.del_time) ):
 
 		# update ego vehicle information
 		self.vel = odomMsg.twist.twist.linear.x;
@@ -125,10 +128,12 @@ class KraussModel:
 		else:
 		    self.ceil_vel = min(self.max_vel, (self.vel + self.max_acc*self.del_time))
 
+		self.floor_vel = max(0, (self.vel - self.max_acc*self.del_time))
+
 
 	        # if velocity is requested by navigation module
                 if (self.active == 1):
-                    lk_vel = min(self.ceil_vel, self.req_nav_vel)
+                    lk_vel = max(min(self.ceil_vel, self.req_nav_vel), self.floor_vel)
 
                     # publish the calculated lane keeping velocity
 		    self.pub.publish(lk_vel)
@@ -138,19 +143,28 @@ class KraussModel:
 	        # if acceleraion is requested by RL policy
                 elif (self.active == 2):
                     lk_vel = (self.vel + self.req_RL_acc*self.del_time)
-                    if ((lk_vel > self.ceil_vel) or (self.req_RL_acc > self.max_acc)):
-                        # set active to -1 indicating an infeasible action
-                        self.active = -1
+                    if ((lk_vel > self.ceil_vel) or (lk_vel < self.floor_vel) or (self.req_RL_acc > self.max_acc) or ((self.req_RL_acc < -self.max_acc))):
+                        if (self.RL_acc_count > 0):
+                            # set active to 0 indicating no more acc/dec can be executed
+                            self.active = 0
+                        else:
+                            # set active to -1 indicating an infeasible action
+                            self.active = -1
+
+                        # reset RL_acc_count
+                        self.RL_acc_count = 0
                         activity_lock.release()
+
                     else: 
    	                # publish the calculated lane keeping velocity
 		        self.pub.publish(lk_vel)
                         rospy.loginfo("Velocity published by vel server to follow RL policy:")
 		        rospy.loginfo(lk_vel)
+                        self.RL_acc_count += 1
 
 	                # switch to inactive
-	                self.active = 0
-                        activity_lock.release()
+	                #self.active = 0
+                        #activity_lock.release()
 
 
 		# update time
@@ -172,58 +186,101 @@ class VelActionServer():
     def __init__(self):
 
         self.a_server = actionlib.SimpleActionServer("move_car/laneKeeping_vel_action_server", MoveCarAction, self.execute_cb, False)
+        self.a_server.register_preempt_callback(self.preemptCB)
         self.a_server.start()
         self.result = MoveCarResult()
 
         self.km = KraussModel() # object of class KraussModel
 
 
+    def preemptCB(self):
+        self.km.active = 0
+        self.km.RL_acc_count = 0
+        rospy.loginfo("Goal preempted in lane keeping velocity action server")
+        
+        activity_lock.release()
+        rospy.loginfo("Releasing lock in preempt callback.")
+
+
     # Goal callback function; goal is a MoveCarAction message
     def execute_cb(self, goal):
 
         rospy.loginfo("Received goal in lane keeping velocity action server")
+        #rate = rospy.Rate(1)
+
         #Extra check that the goal is lane keeping
         if(goal.mcGoal.control_action == 0):
-	        # if action is requested by navigation module
+
+            # if action is requested by navigation module
             if (goal.mcGoal.action_source == 0):
-                self.km.active = 1
-                rate = rospy.Rate(1)
+
+                self.km.last_time = 0
+	        self.km.active = 1
+
                 rospy.loginfo("Executing goal in lane keeping velocity action server")
 
-                while(1):
-                    # check that preempt has not been requested by the client
-                    if self.a_server.is_preempt_requested():
-                        self.km.active = 0
-                        rospy.loginfo("Goal preempted in lane keeping velocity action server")
-                        self.a_server.set_preempted() #########send result?
-                        break
+                #rospy.loginfo("Before first acquire. lock")
+                #rospy.loginfo(activity_lock.locked())
+                activity_lock.acquire(False)
+                #rospy.loginfo("After first acquire. lock")
+                #rospy.loginfo(activity_lock.locked())
+                activity_lock.acquire(True)
+                #rospy.loginfo("After second acquire. lock")
+                #rospy.loginfo(activity_lock.locked())
 
-                    rate.sleep()
+                self.a_server.set_preempted()
 
-	        # if action is requested by RL policy
+                #while(1):
+                #    # check that preempt has not been requested by the client
+                #    if self.a_server.is_preempt_requested():
+                #        self.km.active = 0
+                #        rospy.loginfo("Goal preempted in lane keeping velocity action server")
+                #        self.a_server.set_preempted()
+                #        break
+
+                #    rate.sleep()
+
+            # if action is requested by RL policy
             elif (goal.mcGoal.action_source == 1):
 
                 self.km.req_RL_acc = goal.mcGoal.acc
+                self.km.last_time = 0
                 self.km.active = 2
 
-	            # block while action is still being tested for feasibility or being executed
+                # block while action is still being tested for feasibility or being executed
+                if ((self.km.active != -1) and (self.km.active != 0)):
+                    activity_lock.acquire(False)
+                    activity_lock.acquire(True)
+
                 #while ((self.km.active != -1) and (self.km.active != 0)):
-                #    continue
-                activity_lock.acquire()
 
-	            # if action was successfully executed
-                if (self.km.active == 0):
-                    self.result.mcResult = 1
-                    rospy.loginfo("Reached goal in lane keeping velocity action server")
-                    self.a_server.set_succeeded(self.result)
+                #    if self.a_server.is_preempt_requested():
+	        #        self.km.active = 0
+                #        self.km.RL_acc_count = 0
+                #        rospy.loginfo("Goal preempted in lane keeping velocity action server")
+                #        self.a_server.set_preempted()
+                #        break
 
-	            # if action was found to be infeasible
-                elif (self.km.active == -1):
-                    self.result.mcResult = 0
-                    rospy.loginfo("Infeasible goal in lane keeping velocity action server")
-                    self.a_server.set_aborted(self.result)
+                    #rate.sleep()
+
+                if (not self.a_server.is_preempt_requested()):
+
+                    # if action was successfully executed
+                    if (self.km.active == 0):
+                        self.result.mcResult = 1
+                        rospy.loginfo("Reached goal in lane keeping velocity action server")
+                        self.a_server.set_succeeded(self.result)
+
+                    # if action was found to be infeasible
+                    elif (self.km.active == -1):
+                        self.result.mcResult = 0
+                        rospy.loginfo("Infeasible goal in lane keeping velocity action server")
+                        self.a_server.set_aborted(self.result)
+                else:
+                    self.a_server.set_preempted()
+
         else:
-            rospy.loginfo("Goal is not lane keeping, Please debug your code!")
+            rospy.loginfo("Goal is not lane keeping. Please debug your code!")
             self.a_server.set_aborted()
 
 
