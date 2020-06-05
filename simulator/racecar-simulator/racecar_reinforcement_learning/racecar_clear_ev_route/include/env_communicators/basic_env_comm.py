ec09ae54217c761ee8312111fd55e079f0720faa
Nadine-Amr <s-nadine.amr@zewailcity.edu.eg>
2020-06-05 23:56:32 +0200

    Adding getWindowParams function.

--- simulator/racecar-simulator/racecar_reinforcement_learning/racecar_clear_ev_route/include/env_communicators/basic_env_comm.py
+++ simulator/racecar-simulator/racecar_reinforcement_learning/racecar_clear_ev_route/include/env_communicators/basic_env_comm.py
@@ -16,6 +16,73 @@ class BasicEnvComm:
         self.startSim_client = rospy.ServiceProxy('startSim', startSim)
         self.resetSim_client = rospy.ServiceProxy('resetSim', resetSim)
 
+    def getWindowParams(self):
+
+        ## Communication range ##
+        if rospy.has_param('communicaion_range'):
+            comm_range = rospy.get_param('communicaion_range')
+        else:
+            comm_range = 24
+
+        ## Ambulance parameters ##
+        if rospy.has_param('ambulance/amb_max_vel'):
+            amb_vel_max = rospy.get_param('ambulance/amb_max_vel')
+        else:
+            amb_vel_max = 1 # ambulance max vel
+        ####
+        if rospy.has_param('agent/amb_min_vel'):
+            amb_vel_min = rospy.get_param('agent/amb_min_vel')
+        else:
+            amb_vel_min = 0 # ambulance min vel
+        ####
+        if rospy.has_param('ambulance/amb_max_acc'):
+            emer_max_accel = rospy.get_param('ambulance/amb_max_acc')
+        else:
+            emer_max_accel = 0.0333 # ambulance max acc
+        ####
+        if rospy.has_param('ambulance/rel_amb_y_min'):
+            if (rospy.get_param('ambulance/rel_amb_y_min') < 0):
+                rel_amb_y_min = - min(comm_range, abs(rospy.get_param('ambulance/rel_amb_y_min')))
+            else:
+                rel_amb_y_min = min(comm_range, abs(rospy.get_param('ambulance/rel_amb_y_min')))
+        else:
+            rel_amb_y_min = - min(comm_range, 24) # rear limit of window around agent 
+        ####
+        if rospy.has_param('ambulance/rel_amb_y_max'):
+            if (rospy.get_param('ambulance/rel_amb_y_max') < 0):
+                rel_amb_y_max = - min(comm_range, abs(rospy.get_param('ambulance/rel_amb_y_max')))
+            else:
+                rel_amb_y_max = min(comm_range, abs(rospy.get_param('ambulance/rel_amb_y_max')))
+        else:
+            rel_amb_y_max = min(comm_range, 9) # front limit of window around agent 
+
+        ## Agent parameters ##
+        if rospy.has_param('agent/agent_max_vel'):
+            agent_vel_max = rospy.get_param('agent/agent_max_vel')
+        else:
+            agent_vel_max = 0.5 # agent max vel
+        ####
+        if rospy.has_param('agent/agent_min_vel'):
+            agent_vel_min = rospy.get_param('agent/agent_min_vel')
+        else:
+            agent_vel_min = 0 # agent min vel
+        ####
+        if rospy.has_param('agent/agent_max_acc'):
+            agent_max_acc = rospy.get_param('agent/agent_max_acc')
+        else:
+            agent_max_acc = 0.0167 # agent max acc
+
+        window_params = dict()
+        window_params['rel_amb_y_min'] = rel_amb_y_min
+        window_params['rel_amb_y_max'] = rel_amb_y_max
+        window_params['amb_vel_min'] = amb_vel_min
+        window_params['amb_vel_max'] = amb_vel_max
+        window_params['agent_vel_min'] = agent_vel_min
+        window_params['agent_vel_max'] = agent_vel_max
+        window_params['amb_acc'] = emer_max_accel
+        window_params['agent_acc'] = agent_max_acc
+
+        return window_params
 
     def getState(self, robot_num):
         try:
