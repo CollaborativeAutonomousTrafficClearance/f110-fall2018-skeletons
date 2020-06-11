c83254a47b4906ecf81f3a28b4a24aa9bf0ecf36
Nadine-Amr <s-nadine.amr@zewailcity.edu.eg>
2020-06-11 05:57:57 +0200

    Modifying Stanley controller and doing some further tuning.

--- simulator/racecar-simulator/racecar_control/scripts/stanley_controller.py
+++ simulator/racecar-simulator/racecar_control/scripts/stanley_controller.py
@@ -13,6 +13,7 @@ import os
 #############
 
 VELOCITY = rospy.get_param('max_vel') # m/s
+VEHICLE_LENGTH = 0.58 # m
 
 ###########
 # GLOBALS #
@@ -34,14 +35,20 @@ pub = rospy.Publisher('/drive_parameters', drive_param, queue_size=1)
 # Runs stanley controller and publishes velocity and steering angle
 def callback(data):
 
+    # determine the current yawof the vehicle
+    orientation_q = data.pose.pose.orientation
+    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+    (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+
+
     # Find the path point closest to the vehicle
     min_idx       = 0
     min_dist      = float("inf")
 
     for i in range(len(path_points)):
         dist = np.linalg.norm(np.array([
-                path_points[i][0] - data.pose.pose.position.x,
-                path_points[i][1] - data.pose.pose.position.y]))
+                path_points[i][0] - (data.pose.pose.position.x + (VEHICLE_LENGTH/2)*np.cos(yaw)),
+                path_points[i][1] - (data.pose.pose.position.y + (VEHICLE_LENGTH/2)*np.sin(yaw))]))
         if dist < min_dist:
             min_dist = dist
             min_idx = i
@@ -50,26 +57,30 @@ def callback(data):
         min_idx = min_idx - 1           
     
 
-    # determine the current yawof the vehicle
-    orientation_q = data.pose.pose.orientation
-    orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
-    (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
-
     # calculate the heading
     psi = -yaw + np.arctan2((path_points[min_idx+1][1]-path_points[min_idx][1]),(path_points[min_idx+1][0]-path_points[min_idx][0]))
 
     # calculate an angle proportional to the cross-track error
-    phi1 = np.arctan2((path_points[min_idx][1] - data.pose.pose.position.y), path_points[min_idx][0] - data.pose.pose.position.x)
+    phi1 = np.arctan2((path_points[min_idx][1] - (data.pose.pose.position.y) + (VEHICLE_LENGTH/2)*np.sin(yaw)), path_points[min_idx][0] - (data.pose.pose.position.x + (VEHICLE_LENGTH/2)*np.cos(yaw)))
     phi2 = np.arctan2((path_points[min_idx+1][1] - path_points[min_idx][1]), (path_points[min_idx+1][0] - path_points[min_idx][0]))
 
     phi = (np.mod(phi1 + np.pi, 2 * np.pi) - np.pi) - (np.mod(phi2 + np.pi, 2 * np.pi) - np.pi)
     phi = np.mod(phi + np.pi, 2 * np.pi) - np.pi
 
-    # calculate the re2uired steering angle
+    # calculate the cross-track error
+    err = np.linalg.norm(np.array([
+                path_points[min_idx][0] - (data.pose.pose.position.x + (VEHICLE_LENGTH/2)*np.cos(yaw)),
+                path_points[min_idx][1] - data.pose.pose.position.y+ (VEHICLE_LENGTH/2)*np.sin(yaw)]))
+    err = err * (phi/abs(phi))
+
+    # calculate the required steering angle
     k = 0.005
     ks = 0.1
-    angle    = (k/(ks+VELOCITY))*phi + psi
-    
+
+    err_term = k * (np.arctan2((err), (ks + VELOCITY)))
+    err_term = np.mod(err_term + np.pi, 2 * np.pi) - np.pi
+
+    angle = err_term + psi
     angle = np.clip(angle, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
 
     # publish the drive_param msg
--- simulator/racecar-simulator/racecar_move_car/scripts/laneKeeping_action_server.py
+++ simulator/racecar-simulator/racecar_move_car/scripts/laneKeeping_action_server.py
@@ -399,12 +399,12 @@ def get_waypoint_from_fit(y_upfront, left_fit_pixel, right_fit_pixel, ym_per_pix
 ##------------------------------------------------------------------------------------------##
 ##------------------------------------------------------------------------------------------##
 ##------------------------------------------------------------------------------------------##
-def stanleyController(path_points, velocity, yaw):
+def stanleyController(path_points, velocity, yaw, vehicle_length):
     
     '''
     Applies the lateral controller, Stanley, and returns the required steering angle
 
-    :param path_points: the path points to be followed by the robot
+    :param path_points: the path points to be followed by the robot (in the robot coordinates frame)
     '''
 
     # find the path point closest to the vehicle
@@ -413,8 +413,8 @@ def stanleyController(path_points, velocity, yaw):
 
     for i in range(len(path_points)):
         dist = np.linalg.norm(np.array([
-                path_points[i][0],
-                path_points[i][1]]))
+                path_points[i][0] - (vehicle_length/2)*np.cos(yaw),
+                path_points[i][1] - (vehicle_length/2)*np.sin(yaw)]))
         if dist < min_dist:
             min_dist = dist
             min_idx = i
@@ -426,21 +426,34 @@ def stanleyController(path_points, velocity, yaw):
     # calculate the heading
     psi = -yaw + np.arctan2((path_points[min_idx+1][1]-path_points[min_idx][1]),(path_points[min_idx+1][0]-path_points[min_idx][0]))
 
+    # Logging
     #rospy.loginfo("Yaw:")
     #rospy.loginfo(yaw)
+
     # calculate an angle proportional to the cross-track error
-    phi1 = np.arctan2((path_points[min_idx][1]), path_points[min_idx][0])
+    phi1 = np.arctan2((path_points[min_idx][1] - (vehicle_length/2)*np.sin(yaw)), path_points[min_idx][0] - (vehicle_length/2)*np.cos(yaw))
     phi2 = np.arctan2((path_points[min_idx+1][1] - path_points[min_idx][1]), (path_points[min_idx+1][0] - path_points[min_idx][0]))
 
-    # make sure phi is from -pi to pi
+    # make sure it is from -pi to pi
     phi = (np.mod(phi1 + np.pi, 2 * np.pi) - np.pi) - (np.mod(phi2 + np.pi, 2 * np.pi) - np.pi)
     phi = np.mod(phi + np.pi, 2 * np.pi) - np.pi
 
+    # calculate the cross-track error
+    err = np.linalg.norm(np.array([
+                path_points[min_idx][0] - (vehicle_length/2)*np.cos(yaw),
+                path_points[min_idx][1] - (vehicle_length/2)*np.sin(yaw)]))
+    err = err * (phi/abs(phi))
+
+
     # calculate the required steering angle
-    k = 0.6  # tunable gain 1
+    k = 0.005  # tunable gain 1
     ks = 0.1 # tunable gain 2
 
-    angle    = (k/(ks+velocity))*phi + 1.5*psi
+    err_term = k * (np.arctan2((err), (ks + velocity)))
+    err_term = np.mod(err_term + np.pi, 2 * np.pi) - np.pi
+
+    angle = err_term + psi
+
     angle = np.clip(angle, -0.4189, 0.4189) # 0.4189 radians = 24 degrees because car can only turn 24 degrees max
 
     return angle
@@ -458,6 +471,7 @@ class LaneKeeping:
         self.active = 0 # boolean to indicate if lane keeping is requested 
 
         self.velocity =  0 # lane keeping velocity of vehicle (m/s)
+        self.vehicle_length = 0.58 # length of vehicles (m) - please adjust this value if another vehicle model is used
 
         self.yaw = 0 # vehicle's yaw
 
@@ -541,7 +555,7 @@ class LaneKeeping:
 		path_points = [((wp_x[0]),(wp_y[0]),(float(0.05))), ((wp_x[1]),(wp_y[1]),(0.05)), ((wp_x[2]),(wp_y[2]),(0.05))] # the three generated waypoints
 
 		# apply the Stanley controller
-		angle = stanleyController(path_points, self.velocity, self.yaw)
+		angle = stanleyController(path_points, self.velocity, self.yaw, self.vehicle_length)
 
 		# publish the drive_param message
 		msg = drive_param()
